7750e53c71789d50b661b527d75c3a73434a70ae
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2021-08-28 17:37:29 +0300

    Stage 4 solved

--- Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/task/src/search/Main.java
@@ -2,8 +2,17 @@ package search;
 
 import search.ui.TextInterface;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Collectors;
+
 public class Main implements TextInterface {
-    public static void main(String[] args) {
-        new Application().run();
+
+    public static void main(String[] args) throws IOException {
+        final var fileName = Path.of(args[1]);
+        final var text = Files.lines(fileName).collect(Collectors.toUnmodifiableList());
+
+        new Application(text).run();
     }
 }
