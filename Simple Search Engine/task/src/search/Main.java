03c296d96ef24c5431a1579915ca3398d3b859c1
Jegors Čemisovs <jegors.cemisovs@outlook.lv>
2021-08-28 16:50:15 +0300

    Stage 1 solved

--- Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/task/src/search/Main.java
@@ -1,7 +1,24 @@
 package search;
 
-public class Main {
+import search.ui.TextInterface;
+
+import java.util.regex.Pattern;
+
+import static java.util.stream.Collectors.toUnmodifiableList;
+
+public class Main implements TextInterface {
+    private static final Pattern SPACE = Pattern.compile("\\s+");
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        final var line = scanner.nextLine();
+        final var word = scanner.nextLine();
+        final var words = SPACE.splitAsStream(line).collect(toUnmodifiableList());
+        final var index = words.indexOf(word);
+
+        if (index < 0) {
+            System.out.println("Not found");
+        } else {
+            System.out.println(index + 1);
+        }
     }
 }
